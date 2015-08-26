e8a4b5daf8ff55bdd48c4ff41f2a369cda661335
Oskar Nyberg <oskar@oskarnyberg.com>
2015-08-26 15:46:34 +0200

    Added distance and elevation calculation.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser'),
 app.use(bodyParser.urlencoded({extended: true}));
 app.use("/", serveStatic(__dirname + "/static/"));
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, () => {
     var host = server.address().address;
     var port = server.address().port;
 
--- static/scripts.js
+++ static/scripts.js
@@ -1,4 +1,4 @@
-var map;
+var map, elevator;
 
 $(function() {
     initialize();
@@ -32,22 +32,46 @@ function initialize() {
         }
     }
     map = new google.maps.Map(mapCanvas, mapOptions);
+    elevator = new google.maps.ElevationService;
 }
 
 function loadHills() {
     $.each(hills, function(i, v) {
         var points = extractGpx(gpx[i]);
-        drawPoly(points);
+        var line = drawPoly(points);
 
-        var infoContent = '<h1>' + v.name + '</h1><b>Busstop:</b> ' + v.busstop + '<br /><b>Info:</b> ' + v.extra;
+        var length = line.inMeters();
+        var meters = Math.round(length) + 'm';
+        var kms = (length / 1000).toFixed(1) + 'Km (' + meters + ')';
+        length = length > 1000 ? kms : meters;
+
+        // TODO: Flytta till import delen
+        var locations = {locations: [points[0], points[points.length-1]]};
+        elevator.getElevationForLocations(locations, function(elevation, eStatus) {
+            if (eStatus === google.maps.ElevationStatus.OK) {
+                var decline = Math.round(Math.abs(elevation[0].elevation - elevation[1].elevation)) + 'm';
+            }
+        });
+
+        var infoContent = '<h1 id="firstHeading" class="firstHeading">' + v.name + '</h1><p>' +
+                          '<b>Length:</b> ' + length + '<br />' +
+                          '<b>Decline:</b> ' + v.decline + 'm<br />' +
+                          '<b>Busstop:</b> ' + v.busstop + '<br />' +
+                          '<b>Difficulty:</b> ' + v.difficulty + '/10<br />' +
+                          '<b>Traffic:</b> ' + v.traffic + '/10<br />' +
+                          '<b>Info:</b> ' + v.extra + '</p>';
         var infoWindow = new google.maps.InfoWindow({
-            content: infoContent
+            content: infoContent,
+            maxWidth: 400
         });
 
         var marker = new google.maps.Marker({
             position: points[0],
             map: map,
             title: v.name
+        });
+        line.addListener('click', function() {
+            infoWindow.open(map, marker);
         });
         marker.addListener('click', function() {
             infoWindow.open(map, marker);
@@ -68,12 +92,30 @@ function extractGpx(xml) {
 }
 
 function drawPoly(points) {
-    new google.maps.Polyline({
+    var line = new google.maps.Polyline({
         path: points,
         strokeColor: "#FF00AA",
         strokeOpacity: .7,
         strokeWeight: 3,
         map: map
     });
+
+    return line;
+}
+
+google.maps.Polyline.prototype.inMeters = function(n){
+    var a = this.getPath(n), len = a.getLength(), dist = 0;
+    for(var i=0; i<len-1; i++){
+        dist += a.getAt(i).metersTo(a.getAt(i+1));
+    }
+    return dist;
+}
+google.maps.LatLng.prototype.metersTo = function(a){
+    var e = Math, ra = e.PI/180;
+    var b = this.lat() * ra, c = a.lat() * ra, d = b - c;
+    var g = this.lng() * ra - a.lng() * ra;
+    var f = 2 * e.asin(e.sqrt(e.pow(e.sin(d/2), 2) + e.cos(b) * e.cos
+                (c) * e.pow(e.sin(g/2), 2)));
+    return f * 6378.137 * 1000;
 }
 
