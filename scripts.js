c1a27ab8c08d4bdc90937fc279ee660c5ccefa4a
Oskar Nyberg <oskar@oskarnyberg.com>
2015-08-26 13:04:43 +0200

    Removed data.json

--- scripts.js
+++ scripts.js
@@ -3,7 +3,10 @@ var map;
 function initialize() {
     var mapCanvas = document.getElementById('map');
     var mapOptions = {
-        center: new google.maps.LatLng(57.7, 11.98),
+        center: {
+            lat: 57.7,
+            lng: 11.98
+        },
         zoom: 13,
         mapTypeId: google.maps.MapTypeId.TERRAIN,
         streetViewControl: true,
@@ -24,7 +27,6 @@ function initialize() {
         }
     }
     map = new google.maps.Map(mapCanvas, mapOptions);
-    map.data.loadGeoJson('data.json');
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
