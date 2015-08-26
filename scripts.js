83171199d8937e1d0335dfe2dccc9a9057f98505
Oskar Nyberg <oskar@oskarnyberg.com>
2015-08-26 12:58:57 +0200

    Improved initilization.

--- scripts.js
+++ scripts.js
@@ -3,11 +3,28 @@ var map;
 function initialize() {
     var mapCanvas = document.getElementById('map');
     var mapOptions = {
-        center: new google.maps.LatLng(44.5403, -78.5463),
-        zoom: 8,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        center: new google.maps.LatLng(57.7, 11.98),
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        streetViewControl: true,
+        panControl: false,
+        panControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_TOP
+        },
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_TOP
+        },
+        mapTypeControlOptions: {
+            mapTypeIds: [
+                google.maps.MapTypeId.ROADMAP,
+                google.maps.MapTypeId.SATELLITE,
+                google.maps.MapTypeId.TERRAIN
+            ],
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+        }
     }
     map = new google.maps.Map(mapCanvas, mapOptions);
+    map.data.loadGeoJson('data.json');
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
