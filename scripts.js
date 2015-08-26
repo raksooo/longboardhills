9a9c8009739c856ba84a005e70e0c7547e651887
Oskar Nyberg <oskar@oskarnyberg.com>
2015-08-26 11:49:35 +0200

    Added inititalizing javascript.

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var map;
+
+function initialize() {
+    var mapCanvas = document.getElementById('map');
+    var mapOptions = {
+        center: new google.maps.LatLng(44.5403, -78.5463),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    map = new google.maps.Map(mapCanvas, mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
